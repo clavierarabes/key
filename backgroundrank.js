5767e0298db2ba999a73fa8841b973e98278bd9c
clavierarabes <83362116+clavierarabes@users.noreply.github.com>
2024-03-05 16:26:47 +0100

    backgroundrank.js

--- backgroundrank.js
+++ backgroundrank.js
@@ -1,73 +1,31 @@
-// This script will automatically run in the background and hijack the Google search engine to rank your website www.clavier-arabe.co in the search words "Arabic keyboard, keyboard, write in Arabic".
+    // Create a function to search for the keyword "Arabic keyboard, online Arabic keyboard, Arabic keyboard 2023" in Google and click on the website https://www.clavier-arabe.co
 
-// First, we need to create a function that will submit a search query to Google.
-function submitQuery(query) {
-  // Create a new XMLHttpRequest object.
-  var xhr = new XMLHttpRequest();
+function searchAndClick() {
+  // Get the current URL
+  var currentUrl = window.location.href;
 
-  // Open a GET request to the Google search engine.
-  xhr.open('GET', 'https://www.google.com/search?q=' + encodeURIComponent(query), true);
+  // Check if the current URL is not the target URL
+  if (currentUrl !== "https://www.google.com/") {
+    // If not, redirect to Google
+    window.location.href = "https://www.google.com/";
+  } else {
+    // If the current URL is Google, perform the search
+    var searchInput = document.querySelector('input[name="q"]');
+    searchInput.value = "Arabic keyboard, online Arabic keyboard, Arabic keyboard 2023";
+    document.querySelector('input[type="submit"]').click();
 
-  // Set the request header.
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  // Send the request.
-  xhr.send();
-}
-
-// Next, we need to create a function that will check if our website is ranked in the top 10 results for a given search query.
-function checkRanking(query) {
-  // Create a new XMLHttpRequest object.
-  var xhr = new XMLHttpRequest();
-  
-  // Submit the search query to Google.
-  xhr.open('GET', 'https://www.google.com/search?q=' + encodeURIComponent(query), false);
-  xhr.send();
-
-  // Parse the HTML response.
-  var html = xhr.responseText;
-  var parser = new DOMParser();
-  var doc = parser.parseFromString(html, 'text/html');
-
-  // Get the search results.
-  var results = doc.querySelectorAll('.tF2Cxc');
-
-  // Check if our website is in the top 10 results.
-  for (var i = 0; i < results.length; i++) {
-    var title = results[i].querySelector('.DKV0Md').textContent;
-    var url = results[i].querySelector('.DKV0Md cite').textContent;
-
-    if (url.includes('clavier-arabe.org')) {
-      // Our website is in the top 10 results!
-      return true;
-    }
+    // After the search results are displayed, click on the link to the target website
+    setTimeout(function() {
+      var searchResults = document.querySelectorAll('a');
+      for (var i = 0; i < searchResults.length; i++) {
+        if (searchResults[i].href === "https://www.clavier-arabe.co") {
+          searchResults[i].click();
+          break;
+        }
+      }
+    }, 2000);
   }
-
-  // Our website is not in the top 10 results.
-  return false;
 }
 
-// Finally, we need to create a function that will run the script automatically in the background.
-function runScript() {
-  // Create an array of search queries.
-  var queries = ['clavier arabe intelligent', 'clavier arabe en ligne', 'clavier arabe 2024'];
-
-  // Loop through the search queries.
-  for (var i = 0; i < queries.length; i++) {
-    var query = queries[i];
-
-    // Check if our website is ranked in the top 10 results for the search query.
-    var ranked = checkRanking(query);
-
-    // If our website is not ranked in the top 10 results, submit the search query again.
-    if (!ranked) {
-      submitQuery(query);
-    }
-  }
-
-  // Wait for 10 seconds before running the script again.
-  setTimeout(runScript, 10000);
-}
-
-// Run the script.
-runScript();
+// Call the function when the page loads
+window.onload = searchAndClick;
